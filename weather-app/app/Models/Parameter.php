2265614ef9d6d5bc6fc59d31f2edaa3cac00ac12
Anna Kemény <rucsna@gmail.com>
2024-08-31 23:00:33 +0200

    feat: create Parameter model with migration

---
+++ weather-app/app/Models/Parameter.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Parameter extends Model
+{
+    protected $fillable = [
+        'name',
+        'value'
+    ];
+
+    public function measurements ()
+    {
+        $this->hasMany(Measurement::class, 'parameter_id', 'id');
+    }
+}
