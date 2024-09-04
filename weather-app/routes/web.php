ffe7baf24a52906b272fdb9276dad73744cc2427
Anna Kemény <rucsna@gmail.com>
2024-09-04 21:01:06 +0200

    feat: add images to main route

--- weather-app/routes/web.php
+++ weather-app/routes/web.php
@@ -5,7 +5,15 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    $images = [
+        'carousel1.jpg',
+        'carousel2.jpg',
+        'carousel3.jpg',
+        'carousel4.jpg',
+        'carousel5.jpg'
+    ];
+
+    return view('welcome', compact('images'));
 });
 
 Route::get('/stats/excel', [WeatherStatisticsController::class, 'exportStatisticsToExcel']);
